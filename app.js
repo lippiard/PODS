be63a824c3883544f03a2708eed1690546801775
lippiard <lippiard@seas.upenn.edu>
2018-11-09 15:00:37 -0500

    hide and seek functioning

--- app.js
+++ app.js
@@ -3,11 +3,26 @@
  */
 
 var express = require('express');
-var routes = require('./routes.js')
+var routes = require('./routes.js');
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session')
+
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cookieSession({
+    keys: ['secret1', 'secret2']
+}));
 
 
 app.get('/', routes.get_main);
+app.post('/processchoice', routes.process_choice);
+app.get('/results', routes.get_results);
 
 app.listen(8080);
+//http.createServer(app).listen(8080);
 console.log('Server running on port 8080. Go to http://localhost:8080/')
--- routes.js
+++ routes.js
@@ -3,15 +3,84 @@
  */
 var sd = require('./simuldecision.js');
 
+var choices = {round1: 0, round2: 0, round3: 0, round4: 0};
 
 var getMain = function(req, res) {
-	console.log(sd.get_payoffs('./HideAndSeekJson/evade.json',1,1));
-	res.render('main.ejs', {});
+	var round = req.query.round;
+	var game = null;
+	var player = null;
+	if (round == 1 || round == null) {
+		choices.round1 = 0;
+		choices.round2 = 0;
+		choices.round3 = 0;
+		choices.round4 = 0;
+		game = "Evade";
+		player = "Hider";
+		req.session.round = 1;
+	} else if (round == 2) {
+		game = "Evade";
+		player = "Seeker";
+		req.session.round = 2;
+	} else if (round == 3) {
+		game = "Find";
+		player = "Hider";
+		req.session.round = 3;
+	} else if (round == 4) {
+		game = "Find";
+		player = "Seeker";
+		req.session.round = 4;
+	}
+	res.render('main.ejs', {game: game, player: player});
 }
 	
+var processChoice = function(req, res) {
+	var thischoice = req.body.choice;
+	var round = req.session.round;
+	if (round == 1 || round == null) {
+		choices['round'+round] = thischoice;
+		res.redirect("/?round=2");
+	} else if (round == 2) {
+		choices['round'+round] = thischoice;
+		res.redirect("/?round=3");
+	} else if (round == 3) {
+		choices['round'+round] = thischoice;
+		res.redirect("/?round=4");
+	} else if (round == 4) {
+		choices['round'+round] = thischoice;
+		res.redirect("/results");
+	} else {
+		res.redirect("/");
+	}
+};
+
+var getResults = function(req, res) {
+	var evadePayoffs = sd.get_payoffs('./HideAndSeekJson/evade.json', 
+			choices.round1, choices.round2);
+	var findPayoffs = sd.get_payoffs('./HideAndSeekJson/find.json',
+			choices.round3, choices.round4);
+	var evadeWinner;
+	if (evadePayoffs[0] == 1) {
+		evadeWinner = "Hider";
+	} else if (evadePayoffs[1] == 1) {
+		evadeWinner = "Seeker";
+	}
+	var findWinLoss;
+	if (findPayoffs[1] == 1) {
+		findWinLoss = 'win';
+	} else {
+		findWinLoss = 'lose';
+	}
 	
+	//above is not used right now
+	res.render('results.ejs', choices);
+	
+};
+
+
 var routes = {
-	get_main: getMain	
+	get_main: getMain,
+	process_choice: processChoice,
+	get_results: getResults
 };
 
 module.exports = routes;
