77040c367d27c6e48d42543594b18257dbb578ab
lippiard <lippiard@seas.upenn.edu>
2018-12-05 11:08:03 -0500

    initial socket setup

--- app.js
+++ app.js
@@ -6,18 +6,14 @@ var express = require('express');
 var routes = require('./routes.js');
 var app = express();
 var bodyParser = require('body-parser');
-//var cookieParser = require('cookie-parser');
-//var cookieSession = require('cookie-session')
 var session = require('express-session')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(cookieParser());
-//app.use(cookieSession({
-//    keys: ['secret1', 'secret2']
-//}));
 app.use(express.static(__dirname + '/img/')); // for getting images from local files
 app.use(express.static(__dirname + '/views/')); // for loading ejs parts into other ejs
 app.use(express.static(__dirname + '/node_modules/ejs/')); //for importing ejs into html dynamically
@@ -41,5 +37,13 @@ app.get('/pod', routes.get_pod);
 app.get('/getsessions', routes.get_sessions);
 app.get('/main', routes.get_main);
 
-app.listen(8080);
-console.log('Server running on port 8080. Go to http://localhost:8080/')
+io.on('connection', function(socket) {
+	console.log('a user connected');
+});
+
+http.listen(8080, function() {
+	console.log('listening on port 8080');
+});
+
+//app.listen(8080);
+//console.log('Server running on port 8080. Go to http://localhost:8080/')
