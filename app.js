7ded8587ca278916a8518ad9b98ac61924d907ad
lippiard <lippiard@seas.upenn.edu>
2019-02-23 20:09:35 -0500

    fixed queue

--- app.js
+++ app.js
@@ -47,8 +47,13 @@ var queue = []; //list of sockets waiting to be matched
 
 //add socket to queue or pair with socket already in queue
 var pairSockets = function(socket, sid, username) {
-	if (queue.length > 0) {
-		var waitingSocket = queue.pop();
+	var waiting = queue.find(function(el) {
+		if (el.sessionID === sid) {
+			return el;
+		}
+	});
+	if (waiting) {
+		var waitingSocket = waiting.socket;
 		var room = sid+'#'+waitingSocket.id+'#'+socket.id; //name rooms as sessionID#socket1#socket2
 		waitingSocket.join(room);
 		socket.join(room);
@@ -65,7 +70,7 @@ var pairSockets = function(socket, sid, username) {
 		waitingSocket.emit('start game', {role: role1, room: room});
 		socket.emit('start game', {role: role2, room: room});
 	} else {
-		queue.push(socket);
+		queue.push({socket: socket, sessionID: sid});
 	}
 };
 
